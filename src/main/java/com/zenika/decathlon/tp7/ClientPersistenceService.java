b5c9533d4c3fc4552622ab36359373feb6341f47
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-04-01 09:08:04 +0200

    Valid database tests !

--- src/main/java/com/zenika/decathlon/tp7/ClientPersistenceService.java
+++ src/main/java/com/zenika/decathlon/tp7/ClientPersistenceService.java
@@ -1,13 +1,31 @@
 package com.zenika.decathlon.tp7;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public class ClientPersistenceService {
+	private static final Logger LOGGER = Logger.getLogger(ClientPersistenceService.class.getName());
 
 	private Connection connection;
+	private Properties statements;
 
 	public ClientPersistenceService(Connection connection) {
 		this.connection = connection;
 	}
 
+	public void start() throws IOException, SQLException {
+		loadStatements();
+		Statement init = connection.createStatement();
+		init.executeUpdate(statements.getProperty("init"));
+	}
+
+	private void loadStatements() throws IOException {
+		statements = new Properties();
+		statements.loadFromXML(getClass().getResourceAsStream("statements.xml"));
+	}
 }
--- src/test/java/com/zenika/decathlon/tp7/ClientPersistenceStartupTest.java
+++ src/test/java/com/zenika/decathlon/tp7/ClientPersistenceStartupTest.java
@@ -11,30 +11,20 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class ClientPersistenceTest {
-	private static Connection connection;
-	private static ClientPersistenceService clientPersistence;
-
-	@BeforeAll
-	public static void startDatabase() throws ClassNotFoundException, SQLException, IOException {
+class ClientPersistenceStartupTest {
+	@Test
+	public void startDatabase() throws ClassNotFoundException, SQLException, IOException {
 	    Class.forName("org.h2.Driver");
 	    /*
 	     * Initialisation de la connexion h2
 	     * On peut créer une base sur disque avec une autre chaîne d'initialisation
 	     * Voir par exemple http://www.h2database.com/html/cheatSheet.html
 	     */
-	    connection = DriverManager.getConnection("jdbc:h2:./target/h2/ClientPersistenceTest",
+	    Connection connection = DriverManager.getConnection("jdbc:h2:./target/h2/ClientPersistenceTest",
 	            "sa", "");
-	    clientPersistence = new ClientPersistenceService(connection);
+	    ClientPersistenceService clientPersistence = new ClientPersistenceService(connection);
 	    clientPersistence.start();
-	}
-	
-	@AfterAll public void stopDatabase() throws SQLException {
 		connection.close();
 	}
 
-	@Test public void I_can_create_a_client() {
-		Client toTest = new Client("Delsaux", "Nicolas");
-		// TODO persist client in database and load it afterwards
-	}
 }
